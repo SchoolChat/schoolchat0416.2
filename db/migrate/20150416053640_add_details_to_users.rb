88d0a24320294d9d1b7263bfb3ec3299070a7a2c
Haru Okaniwa <haruokny@gmail.com>
2015-04-28 09:37:05 +0900

    [modify] migration add details to users, reference :users, :school (not :school_id)

--- db/migrate/20150416053640_add_details_to_users.rb
+++ db/migrate/20150416053640_add_details_to_users.rb
@@ -8,7 +8,7 @@ class AddDetailsToUsers < ActiveRecord::Migration
     add_column :users, :school2, :string
     add_column :users, :program, :text
     add_column :users, :bio, :text
-    add_reference :users, :school_id, index: true
-    add_foreign_key :users, :school_id
+    add_reference :users, :school, index: true
+    add_foreign_key :users, :school_ids
   end
 end

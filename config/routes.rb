3e8ffe4618b6faaa67d850c7d01ec8d649b846df
Haru Okaniwa <haruokny@gmail.com>
2015-05-28 11:13:14 +0900

    [change] root to welcome index

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   resource :registrations, only: [:new, :create]
   resource :sessions, only: [:new, :create, :destroy]
 
-  root to: 'schools#index'
+  root to: 'welcome#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
